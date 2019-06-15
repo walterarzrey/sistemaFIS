8a6aa06d823fd07ca439daf6e92cbcc678732eec
Walter Eusebio Arzapalo Rey <www.wearcito23@gmail.com>
2019-06-15 14:04:56 -0500

    Primer commit

---
+++ app/Persona.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+    protected $fillable =[
+        'nombre',
+        'tipo_documento',
+        'num_documento',
+        'direccion',
+        'telefono',
+        'email'
+    ];
+}
